36d785c436c2c0a3c1d68a3c11fdb221e6609f51
Gavin Macaulay <gavin@macaulay.co.nz>
2024-09-19 14:52:13 +1200

    Wholesale cleanup of files

--- __init__.py
+++
@@ -1,4 +0,0 @@
-"""Setup pubic API."""
-
-from .swf import pro_ang1, pro_rad1, pro_rad2
-__all__ = ['pro_ang1', 'pro_rad1', 'pro_rad2']
--- param.f90
+++
@@ -1,6 +0,0 @@
-module param
-    integer, parameter :: knd = selected_real_kind(8)
-    logical, parameter :: debug = .true.
-    logical, parameter :: warn = .true.
-    logical, parameter :: output = .true.
-end module param
